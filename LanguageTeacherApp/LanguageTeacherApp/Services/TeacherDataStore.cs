378afa8e6d775abfe8ce8a5fc93088eb77e7131d
Paul Bender <63110737+paulbnder@users.noreply.github.com>
2021-01-06 20:31:18 +0100

    fix subjects assignment

--- LanguageTeacherApp/LanguageTeacherApp/Services/TeacherDataStore.cs
+++ LanguageTeacherApp/LanguageTeacherApp/Services/TeacherDataStore.cs
@@ -14,24 +14,24 @@ namespace LanguageTeacherApp.Services
         {
             List<string> subjectsMathsPhysics = new List<string>(new string[] { "Maths", "Physics" });
             List<string> subjectsMaths = new List<string>(new string[] { "Maths" });
-            List<string> subjectsEnglishSpanish = new List<string>(new string[] { "Maths", "Physics" });
-            List<string> subjectsMathsPhysicsChemistry = new List<string>(new string[] { "Maths", "Physics" });
-            List<string> subjectsEnglishSpanishFrench = new List<string>(new string[] { "Maths", "Physics" });
+            List<string> subjectsEnglishSpanish = new List<string>(new string[] { "English", "Spanish" });
+            List<string> subjectsMathsPhysicsChemistry = new List<string>(new string[] { "Maths", "Physics", "Chemistry" });
+            List<string> subjectsEnglishSpanishFrench = new List<string>(new string[] { "English", "Spanish", "French" });
 
 
             teachers = new List<Teacher>()
             {
-                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Larisa Samuel", NativeLanguage="English", Occupation="Student", Subjects=subjectsMathsPhysics,
+                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Larisa Samuel", Age=25, NativeLanguage="English", Occupation="Student", Subjects=subjectsMathsPhysics,
                 ProfilePictureSource="https://source.unsplash.com/ROJFuWCsfmA/100x100"},
-                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Peter Gray", NativeLanguage="German", Occupation="Student", Subjects=subjectsMathsPhysicsChemistry,
+                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Peter Gray", Age=26, NativeLanguage="German", Occupation="Student", Subjects=subjectsMathsPhysicsChemistry,
                 ProfilePictureSource="https://source.unsplash.com/c_GmwfHBDzk/100x100"},
-                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Loretta Dylan", NativeLanguage="Spanish", Occupation="Librarian", Subjects=subjectsEnglishSpanishFrench,
+                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Loretta Dylan", Age=23, NativeLanguage="Spanish", Occupation="Librarian", Subjects=subjectsEnglishSpanishFrench,
                 ProfilePictureSource="https://source.unsplash.com/JN0SUcTOig0/100x100"},
-                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Brynne Starr", NativeLanguage="English", Occupation="Web Designer", Subjects=subjectsMaths,
+                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Brynne Starr", Age=19, NativeLanguage="English", Occupation="Web Designer", Subjects=subjectsMaths,
                 ProfilePictureSource="https://source.unsplash.com/tTdC88_6a_I/100x100"},
-                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Roland Sandy", NativeLanguage="Japanese", Subjects=subjectsEnglishSpanishFrench,
+                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Roland Sandy", Age=20, NativeLanguage="Japanese", Subjects=subjectsEnglishSpanishFrench,
                 ProfilePictureSource="https://source.unsplash.com/YUu9UAcOKZ4/100x100"},
-                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Ronan Kurtis", NativeLanguage="English", Occupation="Student", Subjects=subjectsEnglishSpanish,
+                new Teacher { Id = Guid.NewGuid().ToString(), Name = "Ronan Kurtis", Age=31, NativeLanguage="English", Occupation="Student", Subjects=subjectsEnglishSpanish,
                 ProfilePictureSource="https://source.unsplash.com/v2aKnjMbP_k/100x100"}
             };
         }
