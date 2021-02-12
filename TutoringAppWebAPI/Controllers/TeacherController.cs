d5340fb6a3571f7ac2218dcf17d1e823bd23457a
Paul Bender <63110737+paulbnder@users.noreply.github.com>
2021-02-12 20:42:34 +0100

    add post method to controler to add teacher

--- TutoringAppWebAPI/Controllers/TeacherController.cs
+++ TutoringAppWebAPI/Controllers/TeacherController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,29 @@ namespace TutoringAppWebAPI.Controllers
         public IActionResult List()
         {
             return Ok(_teacherRepository.GetItemsAsync());
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Teacher teacher)
+        {
+            try
+            {
+                if (teacher == null || !ModelState.IsValid)
+                {
+                    return BadRequest(ErrorCode.TodoItemNameAndNotesRequired.ToString());
+                }
+                bool teacherExists = _teacherRepository.DoesTeacherExist(teacher.ID);
+                if (teacherExists)
+                {
+                    return StatusCode(StatusCodes.Status409Conflict, ErrorCode.TeacherIDInUse.ToString());
+                }
+                _teacherRepository.Insert(teacher);
+            }
+            catch (Exception)
+            {
+                return BadRequest(ErrorCode.CouldNotCreateTeacher.ToString());
+            }
+            return Ok(teacher);
         }
 
     }
