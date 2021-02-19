d591272f526e07e89afdb85835b8ac613a78fc4d
Paul Bender <63110737+paulbnder@users.noreply.github.com>
2021-02-19 21:43:35 +0100

    change resturl

--- TutoringApp/TutoringApp/Constants.cs
+++ TutoringApp/TutoringApp/Constants.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace TutoringApp
 {
     public static class Constants
     {
-        public static string RestUrl = Device.RuntimePlatform == Device.Android ? "https://10.0.2.2:5001/api/todoitems/{0}" : "https://localhost:5001/api/todoitems/{0}";
+        public static string RestUrl = DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000/api/teacher/{0}" : "http://localhost:5000/api/teacher/{0}";
     }
 }
