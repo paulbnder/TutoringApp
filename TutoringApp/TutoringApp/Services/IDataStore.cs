bcb50a9107c88445b8de565e109496502c09cc65
Paul Bender <63110737+paulbnder@users.noreply.github.com>
2021-02-20 21:57:49 +0100

    change add method to return httpstatuscode

--- TutoringApp/TutoringApp/Services/IDataStore.cs
+++ TutoringApp/TutoringApp/Services/IDataStore.cs
@@ -6,7 +6,7 @@ namespace TutoringApp.Services
 {
     public interface IDataStore<T>
     {
-        Task<bool> AddItemAsync(T item);
+        Task<Object> AddItemAsync(T item);
         Task<bool> UpdateItemAsync(T item);
         Task<bool> DeleteItemAsync(string id);
         Task<T> GetItemAsync(string id);
